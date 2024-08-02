c195453f5eac5a3bbe8067ecafef1f77dc16a39f
gupadhyaya <ganeshrvce@gmail.com>
2024-08-02 10:08:39 +0400

    use pointers to batch

--- sequencing.go
+++ sequencing.go
@@ -20,13 +20,13 @@ type SequencerOutput interface {
 	// GetNextBatch returns the next batch of transactions from sequencer to rollup
 	// lastBatch is the last batch of transactions received from the sequencer
 	// returns the next batch of transactions and an error if any from the sequencer
-	GetNextBatch(ctx context.Context, lastBatch Batch) (Batch, error)
+	GetNextBatch(ctx context.Context, lastBatch *Batch) (*Batch, error)
 }
 
 // BatchVerifier provides a method for verifying a batch of transactions received from the sequencer
 type BatchVerifier interface {
 	// VerifyBatch verifies a batch of transactions received from the sequencer
-	VerifyBatch(ctx context.Context, batch Batch) (bool, error)
+	VerifyBatch(ctx context.Context, batch *Batch) (bool, error)
 }
 
 // RollupId is a unique identifier for a rollup chain
