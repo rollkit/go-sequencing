5ce114a24f9795b58bfb62552a8dc10e28a9940a
gupadhyaya <ganeshrvce@gmail.com>
2024-10-17 18:12:30 +0400

    adding multi-rollup test

--- test/multi_rollup_sequencer_test.go
+++ test/multi_rollup_sequencer_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"context"
 	"math"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -153,4 +154,54 @@ func TestMultiRollupSequencer_MultipleRollups(t *testing.T) {
 
 	assert.Equal(t, 1, len(batchRes2.Batch.Transactions))
 	assert.Equal(t, tx2, batchRes2.Batch.Transactions[0])
+}
+
+// TestMultiRollupSequencer tests the MultiRollupSequencer core functionality of
+// submitting transactions, getting the next batch, and verifying a batch
+func TestMultiRollupSequencer(t *testing.T) {
+	// Test with 1 rollup
+	testMultiRollupSequencer(t, 1)
+	// Test with 2 to 5 rollups
+	numRollups := rand.Intn(4) + 2
+	testMultiRollupSequencer(t, numRollups)
+}
+
+func testMultiRollupSequencer(t *testing.T, numRollups int) {
+	sequencer := NewMultiRollupSequencer()
+
+	for i := 0; i < numRollups; i++ {
+		// Submit a transaction
+		rollupId, err := GenerateSecureRandomBytes(10)
+		assert.NoError(t, err)
+		tx, err := GenerateSecureRandomBytes(30)
+		assert.NoError(t, err)
+		req := sequencing.SubmitRollupTransactionRequest{
+			RollupId: rollupId,
+			Tx:       tx,
+		}
+		_, err = sequencer.SubmitRollupTransaction(context.Background(), req)
+		assert.NoError(t, err)
+
+		// Get Next Batch
+		getBatchReq := sequencing.GetNextBatchRequest{
+			RollupId:      rollupId,
+			LastBatchHash: nil,
+			MaxBytes:      math.MaxInt32,
+		}
+		batchRes, err := sequencer.GetNextBatch(context.Background(), getBatchReq)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(batchRes.Batch.Transactions))
+		assert.Equal(t, tx, batchRes.Batch.Transactions[0])
+
+		// Verify the batch
+		bHash, err := batchRes.Batch.Hash()
+		assert.NoError(t, err)
+		verifyReq := sequencing.VerifyBatchRequest{
+			RollupId:  rollupId,
+			BatchHash: bHash,
+		}
+		verifyRes, err := sequencer.VerifyBatch(context.Background(), verifyReq)
+		assert.NoError(t, err)
+		assert.True(t, verifyRes.Status)
+	}
 }
